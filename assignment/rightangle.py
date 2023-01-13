3dbdf85dcfc8ee8b6d666f2955652db524fbfb68
sunepa01 <117924550+sunepa01@users.noreply.github.com>
2023-01-13 08:43:15 +0100

    initial commit

---
+++ assignment/rightangle.py
@@ -0,0 +1,8 @@
+def asterisk(number):
+    i = 1
+    while i < number:
+        print("* " * i)
+        i += 1
+
+
+asterisk(12)
---
+++ assignment/square.py
@@ -0,0 +1,8 @@
+def square(number):
+    i = 1
+    while i <= number:
+        print("* " * number)
+        i += 1
+
+
+square(10)
